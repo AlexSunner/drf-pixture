045c896e99b9ec9107782dda6545613161c3b794
Alexander Sunnerhage <alexsunnerhage@hotmail.com>
2024-06-05 15:23:59 +0000

    .

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,10 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE,
-)
-
+from django.conf import settings
+from datetime import datetime, timezone
 
 @api_view()
 def root_route(request):
@@ -16,22 +13,24 @@ def root_route(request):
 @api_view(['POST'])
 def logout_route(request):
     response = Response()
+    expires = datetime(1970, 1, 1, tzinfo=timezone.utc)
+
     response.set_cookie(
-        key=JWT_AUTH_COOKIE,
+        key=settings.JWT_AUTH_COOKIE,
         value='',
         httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        expires=expires,
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite=settings.JWT_AUTH_SAMESITE,
+        secure=settings.JWT_AUTH_SECURE,
     )
     response.set_cookie(
-        key=JWT_AUTH_REFRESH_COOKIE,
+        key=settings.JWT_AUTH_REFRESH_COOKIE,
         value='',
         httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        expires=expires,
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite=settings.JWT_AUTH_SAMESITE,
+        secure=settings.JWT_AUTH_SECURE,
     )
     return response
