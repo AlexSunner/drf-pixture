3d83de4e0bd0f13d7200e6f6aae6f42168e78dea
Alexander Sunnerhage <alexsunnerhage@hotmail.com>
2024-06-16 17:45:53 +0000

    Create another custom model Audit log

--- admin_logs/admin.py
+++ admin_logs/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import AdminLog
+from .models import AdminLog, AuditLog
 
 @admin.register(AdminLog)
 class AdminLogAdmin(admin.ModelAdmin):
     list_display = ('admin_user', 'action', 'timestamp', 'model_affected', 'object_id')
     search_fields = ('admin_user__username', 'action', 'model_affected')
+    list_filter = ('timestamp',)
+
+@admin.register(AuditLog)
+class AuditLogAdmin(admin.ModelAdmin):
+    list_display = ('user', 'action', 'timestamp')
+    search_fields = ('user__username', 'action')
     list_filter = ('timestamp',)
--- admin_logs/models.py
+++ admin_logs/models.py
@@ -10,3 +10,11 @@ class AdminLog(models.Model):
 
     def __str__(self):
         return f'{self.admin_user.username} performed {self.action} on {self.model_affected} ({self.object_id}) at {self.timestamp}'
+
+class AuditLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='audit_logs')
+    action = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Action by {self.user.username}: {self.action} at {self.timestamp}'
